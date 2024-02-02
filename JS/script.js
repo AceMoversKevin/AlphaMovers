5ff1cecd97a7e59880bfe5b79a90431da07d8104
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-02-02 13:38:38 +1100

    slideshow
    slideshow complete

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1; //tracks the current slide
+showSlides(slideIndex); // showcases the current slide
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n); //changes the slide index
 }
 
 // Thumbnail image controls (if you have them)
@@ -14,10 +14,21 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+
+    for (i = 0; i < dots.length; i++) { //loop to iterate over the thumbnails
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
     slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+
+
+   
 }
